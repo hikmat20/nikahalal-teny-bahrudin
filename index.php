b062e8356ec9f7790c5856371cc592b3ba0cf0d3
hikmat20 <hikmataulia20@gmail.com>
2021-06-09 21:22:17 +0700

    update susunan directory

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html style="height:100%">
 <!--  -->
+
 <head>
+	<title>Wedding John 1 &mdash; Jane</title>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
-	<title>Wedding John 1 &mdash; Jane</title>
-	<link rel="stylesheet" href="style.html">
 	<meta property="og:title" content="The Wedding of John & Jane" />
-	<meta property="og:image" content="poto/metabanner.jpg" />
+	<meta property="og:image" content="photos/metabanner.jpg" />
 	<meta property="og:url" content="" />
 	<meta property="og:site_name" content="01 Januari 2022" />
 	<meta property="og:description" content="01 Januari 2022" />
@@ -22,20 +22,19 @@
 	<link href="https://fonts.googleapis.com/css2?family=Dancing+Script&amp;display=swap" rel="stylesheet">
 	<link rel="preconnect" href="https://fonts.gstatic.com/">
 	<link href="https://fonts.googleapis.com/css2?family=Parisienne&amp;display=swap" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Work+Sans:400,300,600,400italic,700" rel="stylesheet"
-		type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=Work+Sans:400,300,600,400italic,700" rel="stylesheet" type="text/css">
 	<link href="https://fonts.googleapis.com/css?family=Sacramento" rel="stylesheet">
 
-	<link rel="stylesheet" href="css/animate.css">
-	<link rel="stylesheet" href="css/icomoon.css">
-	<link rel="stylesheet" href="css/bootstrap.css">
-	<link rel="stylesheet" href="css/magnific-popup.css">
-	<link rel="stylesheet" href="css/owl.carousel.min.css">
-	<link rel="stylesheet" href="css/owl.theme.default.min.css">
-	<link rel="stylesheet" href="css/style.css">
-	<link rel="stylesheet" href="css/modified.css">
-	<link rel="stylesheet" href="css/swiper.min.css">
-	<script src="js/modernizr-2.6.2.min.js"></script>
+	<link rel="stylesheet" href="assets/css/animate.css">
+	<link rel="stylesheet" href="assets/css/icomoon.css">
+	<link rel="stylesheet" href="assets/css/bootstrap.css">
+	<link rel="stylesheet" href="assets/css/magnific-popup.css">
+	<link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+	<link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="stylesheet" href="assets/css/modified.css">
+	<link rel="stylesheet" href="assets/css/swiper.min.css">
+	<script src="assets/js/modernizr-2.6.2.min.js"></script>
 	<style>
 		body {
 			background-color: rgb(255 250 236);
@@ -43,7 +42,7 @@
 
 		@font-face {
 			font-family: AksaraJawa;
-			src: url(fonts/JAWAPALSU.TTF);
+			src: url(assets/fonts/JAWAPALSU.TTF);
 		}
 	</style>
 
@@ -51,44 +50,35 @@
 
 <body>
 	<audio loop preload="auto" id="myAudio">
-		<source src="poto/kebo-giro.mp3">
+		<source src="medias/kebo-giro.mp3">
 	</audio>
 
-	<a onclick="playAudio()" type="button" id="un-mute" class="float"><img src="../../assets/mute.png"
-			width="30px;"></a>
-	<a onclick="pauseAudio()" type="button" id="mute" class="float"><img src="../../assets/nomute.png"
-			width="30px;"></a>
+	<a onclick="playAudio()" type="button" id="un-mute" class="float"><img src="images/mute.png" width="30px;"></a>
+	<a onclick="pauseAudio()" type="button" id="mute" class="float"><img src="images/nomute.png" width="30px;"></a>
 
 
-	<div class="modal fade right" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalPreviewLabel"
-		aria-hidden="true" style="overflow: hidden;" data-backdrop="-static" data-keyboard="false">
+	<div class="modal fade right" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalPreviewLabel" aria-hidden="true" style="overflow: hidden;" data-backdrop="-static" data-keyboard="false">
 		<div class="modal-dialog-full-width modal-dialog momodel modal-fluid" role="document">
 			<div class="modal-content-full-width modal-content ">
 				<div id="fh5co-header2">
 					<div id="overlay_popup" style="text-align:center; color:white;">
-						<span
-							style="color:white;font-family:Playfair, serif;display:block;font-size:24px;margin-bottom:0px;letter-spacing:2px;margin-top:50px;">
+						<span style="color:white;font-family:Playfair, serif;display:block;font-size:24px;margin-bottom:0px;letter-spacing:2px;margin-top:50px;">
 							The Wedding of
 						</span>
-						<span
-							style="color:white;font-family: AksaraJawa;display:block;font-size:40px;margin-bottom:0px;letter-spacing:-1px;">
+						<span style="color:white;font-family: AksaraJawa;display:block;font-size:40px;margin-bottom:0px;letter-spacing:-1px;">
 							Pria & Wanita
 						</span>
 						<div style="color:#3d2505;font-family: Lora, serif;margin-top:30px;">
-							<span
-								style="color:white;font-family: Montserrat, sans-serif;display:block;font-size:14px;margin-bottom:0px;letter-spacing:2px;">
+							<span style="color:white;font-family: Montserrat, sans-serif;display:block;font-size:14px;margin-bottom:0px;letter-spacing:2px;">
 								Dear Mr/Mrs/Ms
 							</span>
 
-							<p style="color: #ffffff; font-size:25px; font-weight:bold; padding-bottom:0px; border-radius:0px; text-align: center"
-								id="write"></p>
-							<p
-								style="color:white;font-family: Montserrat, sans-serif;display:block;font-size:14px;margin-bottom:0px;letter-spacing:2px;">
+							<p style="color: #ffffff; font-size:25px; font-weight:bold; padding-bottom:0px; border-radius:0px; text-align: center" id="write"></p>
+							<p style="color:white;font-family: Montserrat, sans-serif;display:block;font-size:14px;margin-bottom:0px;letter-spacing:2px;">
 								You are invited to our wedding
 							</p>
 						</div>
-						<button type="button" class="btn" data-dismiss="modal" onclick="playAudio()"
-							style="background-color:#3d2505;color:white;margin-top:0px;">❤ OPEN INVITATION</button>
+						<button type="button" class="btn" data-dismiss="modal" onclick="playAudio()" style="background-color:#3d2505;color:white;margin-top:0px;">❤ OPEN INVITATION</button>
 						<div style="color:white;font-family: Lora, serif;margin-top:50px;bottom:10px;"></div>
 					</div>
 				</div>
@@ -119,10 +109,8 @@
 					<div class="col-md-8 col-md-offset-2 text-center">
 						<div class="display-t">
 							<div class="display-tc animate-box" data-animate-effect="fadeIn">
-								<h1 class="animated growIn slower" data-id="2"
-									style="font-family:Playfair, serif; color:#ffffff">The Wedding of</h1>
-								<h1
-									style="color:white;font-family: AksaraJawa;display:block;font-size:40px;margin-bottom:0px;letter-spacing:-1px;">
+								<h1 class="animated growIn slower" data-id="2" style="font-family:Playfair, serif; color:#ffffff">The Wedding of</h1>
+								<h1 style="color:white;font-family: AksaraJawa;display:block;font-size:40px;margin-bottom:0px;letter-spacing:-1px;">
 									Pria &amp; Wanita</h1>
 								<br>
 								<br>
@@ -130,15 +118,13 @@
 								<br>
 								<br>
 								<p>
-									<a href="https://calendar.google.com/calendar/u/0/r/eventedit?text=Pria+Wanita+Wedding+Day&amp;dates=20210530T09000/20210530T17000&amp;ctz=Asia/Jakarta&amp;details=Kami+akan+sangat+gembira+ketika+kita+bisa+bertemu+di+acara+kami.%3Cbr%3E%3Cbr%3Esalam+hangat%3Cbr%3EPria+-+Wanita&amp;location=%20Hotel%20Kediaman%20Mempelai%20Wanita&amp;pli=1&amp;uid=1521339627addtocalendar&amp;sf=true&amp;output=xml"
-										class="btn btn-default btn-sm" style="color:#3d2505;" target="blank">Save
+									<a href="https://calendar.google.com/calendar/u/0/r/eventedit?text=Pria+Wanita+Wedding+Day&amp;dates=20210530T09000/20210530T17000&amp;ctz=Asia/Jakarta&amp;details=Kami+akan+sangat+gembira+ketika+kita+bisa+bertemu+di+acara+kami.%3Cbr%3E%3Cbr%3Esalam+hangat%3Cbr%3EPria+-+Wanita&amp;location=%20Hotel%20Kediaman%20Mempelai%20Wanita&amp;pli=1&amp;uid=1521339627addtocalendar&amp;sf=true&amp;output=xml" class="btn btn-default btn-sm" style="color:#3d2505;" target="blank">Save
 										thedate</a>
 								</p>
 								<br>
 								<br>
 								<div>
-									<marquee align="center" direction="up" height="50"> <span align="center"
-											style="color:white;font-family: Calibri;display:block;font-size:25px;margin-bottom:0px;letter-spacing:-1px;">
+									<marquee align="center" direction="up" height="50"> <span align="center" style="color:white;font-family: Calibri;display:block;font-size:25px;margin-bottom:0px;letter-spacing:-1px;">
 											<blink>Swipe Up ↑ </blink>
 										</span></marquee>
 								</div>
@@ -151,8 +137,7 @@
 		</header>
 
 		<!-- COUPLE -->
-		<div id="fh5co-couple" class="fh5co-bg"
-			style=" background-image:url(poto/bg06.png);background-repeat: repeat;background-size: cover;">
+		<div id="fh5co-couple" class="fh5co-bg" style=" background-image:url(photos/bg06.png);background-repeat: repeat;background-size: cover;">
 			<div class=" container">
 				<div class="row">
 					<div class="col-md-8 col-md-offset-2 text-center fh5co-heading animate-box">
@@ -165,7 +150,7 @@
 				<div class="couple-wrap animate-box">
 					<div class="couple-half">
 						<div class="groom">
-							<img src="poto/cepepe.jpg" alt="groom" class="img-responsive">
+							<img src="photos/cepepe.jpg" alt="groom" class="img-responsive">
 						</div>
 						<div class="desc-groom">
 
@@ -198,7 +183,7 @@
 					<p class="heart text-center"><i class="icon-heart2" style="color:#3d2505;"></i></p>
 					<div class="couple-half">
 						<div class="bride">
-							<img src="poto/cepewe.jpg" alt="groom" class="img-responsive">
+							<img src="photos/cepewe.jpg" alt="groom" class="img-responsive">
 						</div>
 						<div class="desc-bride">
 							<h3 style="color:white;font-family:AksaraJawa;display:block;font-size:30px;">
@@ -226,13 +211,11 @@
 		</div>
 
 		<!-- countdonw -->
-		<div id="fh5co-gallery" class="fh5co-bg"
-			style=" background-image:url(poto/bg07.jpg);background-repeat: repeat;background-size: cover;">
+		<div id="fh5co-gallery" class="fh5co-bg" style=" background-image:url(photos/bg07.jpg);background-repeat: repeat;background-size: cover;">
 			<div class="container">
 				<div class="couple-wrap animate-box">
 					<div class="col-md-12" style="text-align:center;">
-						<h1 class="text-center"
-							style="font-family:AksaraJawa;display:block;font-size: 42px;color: #3d2505;">
+						<h1 class="text-center" style="font-family:AksaraJawa;display:block;font-size: 42px;color: #3d2505;">
 							Menghitung Hari</h1>
 						<div id="hitungmundur1" class="gaya" style="background-color:#3d2505"></div>
 						<div id="hitungmundur2" class="gaya" style="background-color:#3d2505"></div>
@@ -244,7 +227,7 @@
 		</div>
 
 		<!-- EVENT -->
-		<div id="fh5co-event" class="fh5co-bg" style="background-image:url(poto/banner1.jpg);">
+		<div id="fh5co-event" class="fh5co-bg" style="background-image:url(photos/banner1.jpg);">
 			<div class="overlay"></div>
 			<div class="container">
 				<div class="row">
@@ -307,8 +290,7 @@
 		</div>
 
 		<!-- MAPS, LOCATION -->
-		<div id="fh5co-event" class="fh5co-cover"
-			style=" background-image:url(poto/bg04.jpg);background-repeat: repeat;background-size: cover;">
+		<div id="fh5co-event" class="fh5co-cover" style=" background-image:url(photos/bg04.jpg);background-repeat: repeat;background-size: cover;">
 			<div class="overlay" style="background-color:rgb(0 0 0 / 77%)"></div>
 			<div class="container">
 				<div class="row">
@@ -317,13 +299,10 @@
 						<h2 style="color:white;font-family:AksaraJawa;display:block;font-size:32px;">Lokasi
 							Pernikahan</h2>
 						<div class="text-center">
-							<iframe
-								src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.241993198662!2d107.17965061435197!3d-6.362718964013728!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNsKwMjEnNDUuOCJTIDEwN8KwMTAnNTQuNiJF!5e0!3m2!1sid!2sid!4v1620479513149!5m2!1sid!2sid"
-								width="340" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+							<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.241993198662!2d107.17965061435197!3d-6.362718964013728!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNsKwMjEnNDUuOCJTIDEwN8KwMTAnNTQuNiJF!5e0!3m2!1sid!2sid!4v1620479513149!5m2!1sid!2sid" width="340" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
 						</div>
 						<div class="text-center">
-							<a href="https://goo.gl/maps/xk7vpexCbcCuzeKYA" class="btn btn-default btn-sm"
-								style="color:#3d2505;" target="blank">BUKA PETA</a>
+							<a href="https://goo.gl/maps/xk7vpexCbcCuzeKYA" class="btn btn-default btn-sm" style="color:#3d2505;" target="blank">BUKA PETA</a>
 						</div>
 					</div>
 
@@ -342,7 +321,7 @@
 					menghargai dukungan dan do"anya.
 				<p>
 				<div class="text-center">
-					<img src="PROKES.jpg" width=345" height="235" />
+					<img src="photos/PROKES.jpg" width=345" height="235" />
 				</div>
 
 				<br><br>
@@ -359,29 +338,29 @@
 							<h2 style="color:#3d2505;font-family:AksaraJawa;display:block;font-size:32px;">Gallery
 							</h2>
 
-							<img src="poto/1.jpg" style="width:100%">
+							<img src="photos/1.jpg" style="width:100%">
 							<br><br>
-							<img src="poto/2.jpg" style="width:100%">
+							<img src="photos/2.jpg" style="width:100%">
 							<br><br>
-							<img src="poto/3.jpg" style="width:100%">
+							<img src="photos/3.jpg" style="width:100%">
 							<br><br>
-							<img src="poto/4.jpg" style="width:100%">
+							<img src="photos/4.jpg" style="width:100%">
 							<br><br>
-							<img src="poto/5.jpg" style="width:100%">
+							<img src="photos/5.jpg" style="width:100%">
 							<br><br>
-							<img src="poto/6.jpg" style="width:100%">
+							<img src="photos/6.jpg" style="width:100%">
 							<br><br>
-							<img src="poto/7.jpg" style="width:100%">
+							<img src="photos/7.jpg" style="width:100%">
 							<br><br>
-							<img src="poto/8.jpg" style="width:100%">
+							<img src="photos/8.jpg" style="width:100%">
 							<br><br>
-							<img src="poto/9.jpg" style="width:100%">
+							<img src="photos/9.jpg" style="width:100%">
 							<br><br>
-							<img src="poto/10.jpg" style="width:100%">
+							<img src="photos/10.jpg" style="width:100%">
 							<br><br>
-							<img src="poto/11.jpg" style="width:100%">
+							<img src="photos/11.jpg" style="width:100%">
 							<br><br>
-							<img src="poto/12.jpg" style="width:100%">
+							<img src="photos/12.jpg" style="width:100%">
 							<br><br>
 							</a>
 							</li>
@@ -425,13 +404,11 @@
 
 								<h2 style="color:#3d2505;font-family:AksaraJawa;display:block;font-size:32px;">
 									Hadiah</h2>
-								<h4 class="animated growIn slower" font-size:12px;data-id="2"
-									style="font-family:Dancing+Script; color:#3d2505">Bagi
+								<h4 class="animated growIn slower" font-size:12px;data-id="2" style="font-family:Dancing+Script; color:#3d2505">Bagi
 									keluarga, sahabat dan rekan
 									yang ingin mengirimkan hadiah, bisa dikirimkan melalui tautan berikut:</h4>
 
-								<h4 class="animated growIn slower" font-size:12px;data-id="2"
-									style="font-family:Dancing+Script; color:#3d2505">BCA a/n
+								<h4 class="animated growIn slower" font-size:12px;data-id="2" style="font-family:Dancing+Script; color:#3d2505">BCA a/n
 									Siti Soleha<br>
 									5780775027</h4>
 
@@ -442,21 +419,18 @@
 									<form action="#" method="POST">
 
 										<label>
-											<input type="text" name="Name" class="Input" style="width: 350px"
-												placeholder="Nama Kamu" required>
+											<input type="text" name="Name" class="Input" style="width: 350px" placeholder="Nama Kamu" required>
 										</label>
 										<br>
 										<label><br>
-											<textarea name="Comment" class="Input" style="width: 350px"
-												placeholder="Tulis Ucapan dan Doa" required></textarea>
+											<textarea name="Comment" class="Input" style="width: 350px" placeholder="Tulis Ucapan dan Doa" required></textarea>
 										</label>
 										<br>
-										<input type="submit" name="Submit" value="Kirim" style="width: 350px"
-											class="Submit">
+										<input type="submit" name="Submit" value="Kirim" style="width: 350px" class="Submit">
 
 									</form>
 
-									<img src="floww.png" style="width:100px;"><br>
+									<img src="images/floww.png" style="width:100px;"><br>
 									<b>
 										<h4>Nikahalal Official
 										</h4>
@@ -492,7 +466,7 @@
 											const sampul = jQuery(".modalx").data("sampul");
 											jQuery(".modalx").css("background-image", "url(" + sampul + ")");
 											jQuery("body").css("overflow", "hidden");
-											jQuery(".info_button button").on("click", function () {
+											jQuery(".info_button button").on("click", function() {
 												jQuery(".modalx").addClass("removeModals");
 												jQuery("body").css("overflow", "auto");
 												document.getElementById("song").play();
@@ -514,20 +488,20 @@
 			<a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
 		</div>
 
-		<script src="../../cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-		<script src="../../maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
-		<script src="js/jquery.easing.1.3.js"></script>
-		<script src="js/jquery.waypoints.min.js"></script>
-		<script src="js/owl.carousel.min.js"></script>
-		<script src="js/jquery.countTo.js"></script>
-		<script src="js/jquery.stellar.min.js"></script>
-		<script src="js/jquery.magnific-popup.min.js"></script>
-		<script src="js/magnific-popup-options.js"></script>
-		<script src="js/simplyCountdown.js"></script>
-		<script src="js/main.js"></script>
-		<script src="js/index.js"></script>
-		<script src="../../cdn.rawgit.com/bungfrangki/efeksalju/2a7805c7/efek-salju.js" type="text/javascript"></script>
+		<script src="assets/js/jquery.easing.1.3.js"></script>
+		<script src="assets/js/jquery.waypoints.min.js"></script>
+		<script src="assets/js/owl.carousel.min.js"></script>
+		<script src="assets/js/jquery.countTo.js"></script>
+		<script src="assets/js/jquery.stellar.min.js"></script>
+		<script src="assets/js/jquery.magnific-popup.min.js"></script>
+		<script src="assets/js/magnific-popup-options.js"></script>
+		<script src="assets/js/simplyCountdown.js"></script>
+		<script src="assets/js/main.js"></script>
+		<script src="assets/js/index.js"></script>
+		<script src="https://cdn.rawgit.com/bungfrangki/efeksalju/2a7805c7/efek-salju.js" type="text/javascript"></script>
 
 
 		<script>
@@ -550,6 +524,7 @@
 		</script>
 		<script>
 			Hitung();
+
 			function Hitung() {
 
 				tahun = 2021;
@@ -559,7 +534,7 @@
 				menit = 00;
 				detik = 00;
 
-				setTimeout(function () {
+				setTimeout(function() {
 					tglTarget = new Date(tahun, (bulan - 1), hari, jam, menit, detik, 00);
 					tglSkrg = new Date();
 					tglSkrg = new Date(tglSkrg.getFullYear(), tglSkrg.getMonth(), tglSkrg.getDate(), tglSkrg.getHours(), tglSkrg.getMinutes(), tglSkrg.getSeconds(), 00, 00);
@@ -585,18 +560,18 @@
 				document.getElementById("hitungmundur4").innerHTML = "" + detik + "<span>Detik</span>";
 				Hitung();
 			}
-
 		</script>
 
 		<script>
 			document.getElementById("mute").style.display = "none";
-
 			var x = document.getElementById("myAudio");
+
 			function playAudio() {
 				x.play();
 				document.getElementById("un-mute").style.display = "none";
 				document.getElementById("mute").style.display = "inline-block";
 			}
+
 			function pauseAudio() {
 				x.pause();
 				document.getElementById("mute").style.display = "none";
