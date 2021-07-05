29f94a8d6ea5af46c93e0dca0959b5a328ab39ce
hikmat20 <hikmataulia20@gmail.com>
2021-07-05 19:44:01 +0700

    update alamat unduh mantu

--- index.php
+++ index.php
@@ -276,7 +276,7 @@
 										</div>
 										<p>
 											<label style="font-weight:normal; font-size:20px;">Kediaman Mempelai Pria</label>
-											<br><span style="color:#ffffff;">Jl. Karangasem, Sumbersari, Badas, Kec. Badas, Kediri, Jawa Timur</span>
+											<br><span style="color:#ffffff;">Jl. Kunjang Badas, Desa Balongrejo, Kec. Badas, Kediri, Jawa Timur</span>
 										</p>
 									</div>
 								</div>
